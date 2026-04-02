9de3cc3d473a73ab8cc2b8735345915f6b193bee
Keshav Ghai <keshavghai89@gmail.com>
2026-04-03 01:27:27 +0530

    done

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,10 +1,22 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./ecom.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ecom.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# SQLite (local)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    # Supabase / PostgreSQL (Render)
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"sslmode": "require"}
+    )
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 Base = declarative_base()
