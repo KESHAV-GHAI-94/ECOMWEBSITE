4f4e3a4f4ce6c2b991e0d42d1ff8d6320eceaf0e
Keshav Ghai <keshavghai89@gmail.com>
2026-04-03 21:43:25 +0530

    done

--- backend/config/email_config.py
+++ backend/config/email_config.py
@@ -2,7 +2,7 @@ from fastapi_mail import ConnectionConfig
 
 conf = ConnectionConfig(
     MAIL_USERNAME="keshavghai94@gmail.com",
-    MAIL_PASSWORD="jlzmztvnsmlzcrbr",
+    MAIL_PASSWORD="lvpegssjgiczvkaq",
     MAIL_FROM="keshavghai94@gmail.com",
     MAIL_PORT=587,
     MAIL_SERVER="smtp.gmail.com",
