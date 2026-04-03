630cd9ddb256e46116b3cd05c419c6e89318e90c
Keshav Ghai <keshavghai89@gmail.com>
2026-04-03 22:15:55 +0530

    done

--- frontend/src/components/Shop/ShopmainLayout.jsx
+++ frontend/src/components/Shop/ShopmainLayout.jsx
@@ -3,7 +3,7 @@ import Navbar from "./Navbar";
 
 const ShopmainLayout = () => {
   return (
-    <div className=" flex flex-col">
+    <div className="flex flex-col min-h-screen">
       <Navbar />
       <main className="flex-1 overflow-y-auto">
         <Outlet />
