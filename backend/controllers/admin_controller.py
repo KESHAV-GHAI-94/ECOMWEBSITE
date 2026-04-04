cefa653d2150aa33d70a92df29ce3a26816a9bc3
Keshav Ghai <keshavghai89@gmail.com>
2026-04-04 16:02:51 +0530

    done

--- backend/controllers/admin_controller.py
+++ backend/controllers/admin_controller.py
@@ -65,7 +65,8 @@ async def create_product(
 
     image_url = None
     if image and hasattr(image, "filename") and image.filename:
-        upload_result = cloudinary.uploader.upload(image.file)
+        file_bytes = await image.read()
+        upload_result = cloudinary.uploader.upload(file_bytes)
         image_url = upload_result.get("secure_url")
 
     new_product = Product(
@@ -133,7 +134,8 @@ async def update_product(
     db_product.p_discount = p_discount
     db_product.p_category = p_category
     if image and hasattr(image, "filename") and image.filename:
-        upload_result = cloudinary.uploader.upload(image.file)
+        file_bytes = await image.read()
+        upload_result = cloudinary.uploader.upload(file_bytes)
         db_product.p_image = upload_result.get("secure_url")
     db.commit()
     db.refresh(db_product)
